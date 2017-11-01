383d5a68eb4117bf415dfeda1af52c626f9842d7
Smurfa <pontus.franzen@tretton37.com>
2017-11-01 13:55:55 +0100

    Add CsvImporter.

---
+++ DataImporter/Csv/CsvImporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using DataImporter.Model;
+
+namespace DataImporter.Csv
+{
+    public class CsvImporter
+    {
+        private CsvReader _reader;
+
+        public IEnumerable<ShareHistory> ReadFile(string filepath, string name)
+        {
+            using (var reader = File.OpenText(filepath))
+            {
+                _reader = new CsvReader(reader, new Configuration{ Delimiter = ";", CultureInfo = new CultureInfo("sv-SE") });
+                _reader.Read();
+
+                return _reader.GetRecords<dynamic>()
+                    .Select(record => new ShareHistory
+                    {
+                        ClosingValue = _reader.GetField<float>(6),
+                        Date = _reader.GetField<DateTime>(0),
+                        Name = name
+                    }).ToList();
+            }
+        }
+    }
+}
---
+++ DataImporter/Model/ShareHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataImporter.Model
+{
+    public class ShareHistory
+    {
+        public string Name;
+        public DateTime Date;
+        public float ClosingValue;
+    }
+}
