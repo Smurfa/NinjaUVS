ccb9390144d15c94146350e4b830734e2616a921
Smurfa <pontus.franzen@tretton37.com>
2017-11-03 13:59:35 +0100

    Add EnumerableExtension DistinctBy.

---
+++ NinjaUVS/IEnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NinjaUVS
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector)
+        {
+            var seenKeys = new HashSet<TKey>();
+            foreach (var element in source)
+            {
+                if (seenKeys.Add(selector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
