7be15e2de42cdafd409c99eacba0894e4a1a4874
Smurfa <pontus.franzen@tretton37.com>
2017-11-03 15:04:40 +0100

    Add ownership. Add output of subscriptions and ownership.

--- NinjaUVS/Program.cs
+++ NinjaUVS/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using DataUtilities;
 using DataUtilities.Csv;
 using DataUtilities.Models;
@@ -13,6 +15,9 @@ namespace NinjaUVS
         private static IDictionary<string, IEnumerable<ShareHistoryPoint>> _shares;
         private static IEnumerable<TransactionBase> _transactions;
 
+        private static IEnumerable<Subscription> _subscriptions;
+        private static IEnumerable<Ownership> _ownerships;
+
         internal static void Main(string[] args)
         {
             try
@@ -23,7 +28,11 @@ namespace NinjaUVS
 
                 var calcSubscriptions = new SubscriptionAlgorithm(_transactions, _shares);
                 _subscriptions = calcSubscriptions.RunTransactions();
-                
+
+                _ownerships = SummarizeOwnerships(_subscriptions);
+
+                var savior = new DataSaver(new CsvExporter()); // :DD
+                SaveData(savior);
                 Logger.Log(LogLevel.Info, "Shutting down");
             }
             catch (Exception e)
@@ -36,6 +45,24 @@ namespace NinjaUVS
         {
             _shares = loader.LoadSharesHistory(Properties.Settings.Default.HistoryPath);
             _transactions = loader.LoadTransactions(Properties.Settings.Default.TransactionPath);
+        }
+
+        private static void SaveData(DataSaver saver)
+        {
+            saver.Save(_ownerships, "Ownership.csv");
+            saver.Save(_subscriptions, "Subscriptions.csv");
+        }
+
+        private static IEnumerable<Ownership> SummarizeOwnerships(IEnumerable<Subscription> subscriptions)
+        {
+            return subscriptions.DistinctBy(x => x.Member)
+                .Select(subscription => new Ownership
+                {
+                    Name = subscription.Member,
+                    Paid = subscriptions.Where(x => x.Member == subscription.Member).Aggregate(0.0f, (current, next) => current + next.Payment),
+                    Units = subscriptions.Where(x => x.Member == subscription.Member).Aggregate(0.0f, (current, next) => current + next.PurchasedUnits)
+                })
+                .ToList();
         }
     }
 }
