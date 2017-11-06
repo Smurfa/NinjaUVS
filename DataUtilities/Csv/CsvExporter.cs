7f5d3afedb9d9d30c0f9a6be13745ba9bbd41049
Smurfa <pontus.franzen@tretton37.com>
2017-11-06 12:33:56 +0100

    Update exporter to use semicolon as delimiter and Swedish as culture.

--- DataUtilities/Csv/CsvExporter.cs
+++ DataUtilities/Csv/CsvExporter.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using CsvHelper;
+using CsvHelper.Configuration;
 using NLog;
 
 namespace DataUtilities.Csv
@@ -17,7 +19,7 @@ namespace DataUtilities.Csv
         public void Export<T>(IEnumerable<T> data, string outputPath)
         {
             using (var textWriter = File.CreateText(outputPath))
-            using (var csv = new CsvWriter(textWriter))
+            using (var csv = new CsvWriter(textWriter, new Configuration { Delimiter = ";", CultureInfo = new CultureInfo("sv-SE") }))
             {
                 csv.WriteRecords(data);
             }
