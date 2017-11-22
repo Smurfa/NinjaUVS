0c4d89530625087db2645ed609e125dfd8fda9c3
Smurfa <pontus.franzen@tretton37.com>
2017-11-22 15:31:49 +0100

    Add unit tests for NanNullFloatConverter.

---
+++ NinjaUVSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NinjaUVSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NinjaUVSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f78632d-ee8a-4da5-9d07-a30bd07d97f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NinjaUVSTest/TestNanNullFloatConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CsvHelper.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataUtilities.Csv;
+
+namespace NinjaUVSTest
+{
+    [TestClass]
+    public class TestNanNullFloatConverter
+    {
+        private readonly NanNullFloatConverter _converter = new NanNullFloatConverter();
+
+        [TestMethod]
+        public void Float_ConvertFromString_NullInput()
+        {
+            Assert.IsNull(_converter.ConvertFromString(null, null, null));
+        }
+
+        [TestMethod]
+        public void Float_ConvertFromString_ValidInput()
+        {
+            var value = (float?)_converter.ConvertFromString("1,5", null, null);
+            Assert.IsTrue(value == 1.5f);
+        }
+
+        [TestMethod]
+        public void Float_ConvertFromString_InvalidOrEmptyInput()
+        {
+            Assert.IsNull(_converter.ConvertFromString("-", null, null));
+
+            Assert.IsNull(_converter.ConvertFromString(string.Empty, null, null));
+        }
+
+        [TestMethod]
+        public void Float_ConvertToString_ValidInput()
+        {
+            Assert.IsTrue(_converter.ConvertToString(1.5f, null, null) == "1,5");
+        }
+
+        [TestMethod]
+        public void Float_ConvertToString_NullInput()
+        {
+            Assert.IsTrue(_converter.ConvertToString(null, null, null) == "-");
+        }
+    }
+}
